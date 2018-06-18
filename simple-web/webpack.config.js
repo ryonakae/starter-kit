fee195d14e153b412afa626017a50312ec833c75
Ryo Nakae <ryo.n.1129@gmail.com>
2018-06-19 05:01:22 +0900

    browserSync -> webpack-serve

--- simple-web/webpack.config.js
+++ simple-web/webpack.config.js
@@ -3,146 +3,145 @@ const path = require('path')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
-// common config
-module.exports = (env, argv) => {
-  const IS_DEV = argv.mode === 'development'
+const MODE = process.env.WEBPACK_SERVE ? 'development' : 'production'
+const IS_DEV = MODE === 'development'
 
-  return {
-    entry: {
-      index: path.join(__dirname, 'src/scripts/index.js')
-    },
-    output: {
-      filename: '[name].js',
-      path: path.join(__dirname, 'dist')
-    },
-    module: {
-      rules: [
-        // fonts
-        {
-          test: /\.(otf|eot|ttf|woff|woff2)$/,
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]',
-            outputPath: 'fonts/'
-          }
-        },
-        // images
-        {
-          test: /\.(jpe?g|png|bmp|gif|svg)$/,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                name: '[name].[ext]',
-                outputPath: 'images/'
-              }
-            },
-            {
-              loader: 'image-webpack-loader',
-              options: {
-                bypassOnDebug: true,
-                mozjpeg: {
-                  quality: 85,
-                  progressive: true
-                },
-                pngquant: {
-                  quality: '80-90',
-                  speed: 1,
-                  floyd: 0
-                },
-                optipng: {
-                  enabled: false
-                },
-                gifsicle: {
-                  interlaced: false
-                },
-                svgo: {
-                  plugins: [{ removeViewBox: false }]
-                }
-              }
-            }
-          ]
-        },
-        // css
-        {
-          test: /\.css$/,
-          use: [
-            { loader: 'style-loader', options: { sourceMap: true } },
-            {
-              loader: 'css-loader',
-              options: { sourceMap: true, importLoaders: 1 }
-            },
-            { loader: 'postcss-loader', options: { sourceMap: true } }
-          ]
-        },
-        // js
-        {
-          test: /\.js$/,
-          loader: 'babel-loader',
-          exclude: /node_modules/
-        },
-        // ejs
-        {
-          test: /\.ejs$/,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                name: '[name].html'
-              }
-            },
-            'extract-loader',
-            'html-loader',
-            {
-              loader: 'ejs-html-loader',
-              options: {
-                mode: argv.mode
-              }
-            }
-          ]
-        }
-      ]
-    },
-    resolve: {
-      alias: {
-        '@': path.join(__dirname, 'src')
-      }
-    },
-    plugins: [
-      new CopyWebpackPlugin([
-        {
-          from: path.join(__dirname, 'src/static'),
-          to: './',
-          ignore: ['.DS_Store', '.gitkeep']
-        }
-      ])
-    ],
-    optimization: {
-      splitChunks: {
-        cacheGroups: {
-          vendor: {
-            test: /node_modules/,
-            name: 'vendor',
-            chunks: 'initial',
-            enforce: true
-          }
+module.exports = {
+  mode: MODE,
+  entry: {
+    index: path.join(__dirname, 'src/scripts/index.js')
+  },
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      // fonts
+      {
+        test: /\.(otf|eot|ttf|woff|woff2)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'fonts/'
         }
       },
-      minimizer: IS_DEV
-        ? []
-        : [
-          new UglifyJSPlugin({
-            sourceMap: true,
-            uglifyOptions: {
-              compress: {
-                warnings: false,
-                drop_console: true
-              },
-              comments: false
+      // images
+      {
+        test: /\.(jpe?g|png|bmp|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images/'
             }
-          })
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              disable: IS_DEV,
+              mozjpeg: {
+                quality: 85,
+                progressive: true
+              },
+              pngquant: {
+                quality: '80-90',
+                speed: 1,
+                floyd: 0
+              },
+              optipng: {
+                enabled: false
+              },
+              gifsicle: {
+                interlaced: false
+              },
+              svgo: {
+                plugins: [{ removeViewBox: false }]
+              }
+            }
+          }
         ]
+      },
+      // css
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader', options: { sourceMap: true } },
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true, importLoaders: 1 }
+          },
+          { loader: 'postcss-loader', options: { sourceMap: true } }
+        ]
+      },
+      // js
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      // ejs
+      {
+        test: /\.ejs$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].html'
+            }
+          },
+          'extract-loader',
+          'html-loader',
+          {
+            loader: 'ejs-html-loader',
+            options: {
+              mode: MODE
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      '@': path.join(__dirname, 'src')
+    }
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: path.join(__dirname, 'src/static'),
+        to: './',
+        ignore: ['.DS_Store', '.gitkeep']
+      }
+    ])
+  ],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /node_modules/,
+          name: 'vendor',
+          chunks: 'initial',
+          enforce: true
+        }
+      }
     },
-    devtool: IS_DEV ? 'inline-source-map' : 'none'
-  }
+    minimizer: IS_DEV
+      ? []
+      : [
+        new UglifyJSPlugin({
+          sourceMap: true,
+          uglifyOptions: {
+            compress: {
+              warnings: false,
+              drop_console: true
+            },
+            comments: false
+          }
+        })
+      ]
+  },
+  devtool: IS_DEV ? 'inline-source-map' : 'none'
 }
